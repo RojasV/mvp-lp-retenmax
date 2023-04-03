38daca00eb0a954a4a1d19d09c09dd7ef0dcb184
RojasV <rojasvladimir.vr@gmail>
2023-04-02 21:30:42 -0300

    adaptando lp para mobile

---
+++ js/mailSender.js
@@ -0,0 +1,56 @@
+class MailSender {
+    constructor() {
+        this.mailEl = document.querySelector('.php-email-form')
+        this.submitBtn = document.querySelector('.mail-submit')
+        this.sendMail()
+
+    }
+
+    async sendMail() {
+        this.mailEl.addEventListener('submit', e => {
+            e.preventDefault()
+
+            this.submitBtn.setAttribute("disabled", true)
+
+            const formData = new FormData(this.mailEl)
+
+            let celphoneHandledToStringNumber = "55" + formData.get("celphone").replace("(", "").replace(")", "").replace(" ", "").replace("-", "")
+
+            const templateParams = {
+                from_name: formData.get("name"),
+                email: formData.get("email"),
+                celphone: celphoneHandledToStringNumber
+            }
+
+            emailjs.send('service_0lhss64', "template_4ndxcal", templateParams, 'gQaGWN1YCHugVnOdV')
+                .then(() => {
+
+                    this.successAlertHandler(templateParams.from_name)
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+        })
+    }
+
+   
+
+    successAlertHandler(user) {
+        Swal.fire({
+                title: `Ficamos felizes pelo seu interesse ${user}!`,
+                text: 'Recebemos o seu email e te responderemos em no máximo 1 minutinho!',
+                icon: 'success',
+                confirmButtonText: "ok"
+            })
+            .then(() => {
+                this.submitBtn.removeAttribute("disabled")
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+
+
+}
+
+window.app = new MailSender()
